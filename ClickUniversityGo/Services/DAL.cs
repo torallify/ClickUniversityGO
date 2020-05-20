376cf40b018948c16e70154c659aa8eb63146147
Sameer Siddiqui <sameersiddiqui@quickenloans.com>
2020-05-20 11:12:22 -0400

    Added University DAL methods

--- ClickUniversityGo/Services/DAL.cs
+++ ClickUniversityGo/Services/DAL.cs
@@ -23,16 +23,55 @@ namespace ClickUniversityGo.Services
             string queryString = "SELECT * FROM Universities";
             IEnumerable<University> Universities = conn.Query<University>(queryString);
 
+            conn.Close();
+
             return Universities;
         }
 
-
-        // ************************ Favorites ********************
-        public IEnumerable<Favorite> GetAllFavorites()
+        public University GetUniversityByID(int id)
         {
-            string queryString = "SELECT * FROM Favorites";
-            IEnumerable<Favorite> Favorites = conn.Query<Favorite>(queryString);
+            string queryString = "SELECT * FROM Universities WHERE ID = @id";
 
+            University result = conn.QueryFirst<University>(queryString, new { id = id });
+            conn.Close();
+            return result;
+        }
+
+        public IEnumerable<University> GetUniversityByMaxACT(int score)
+        {
+            string queryString = "SELECT * FROM Universities WHERE ActComposite <= @score";
+            IEnumerable<University> Universities = conn.Query<University>(queryString, new { score = score });
+
+            conn.Close();
+
+            return Universities;
+        }
+
+        public IEnumerable<University> GetUniversityByMaxCostOnCampusInState(int cost)
+        {
+            string queryString = "SELECT * FROM Universities WHERE CostOnCampusInState <= @cost";
+            IEnumerable<University> Universities = conn.Query<University>(queryString, new { cost = cost });
+
+            conn.Close();
+
+            return Universities;
+        }
+
+        public IEnumerable<University> GetUniversityByMaxPopulation(int pop)
+        {
+            string queryString = "SELECT * FROM Universities WHERE ((NumBachelor/(GraduationRate/100)) * 4) <= @pop";
+            IEnumerable<University> Universities = conn.Query<University>(queryString, new { pop = pop });
+
+            conn.Close();
+
+            return Universities;
+        }
+        // ************************ Favorites ********************
+        public IEnumerable<Favorite> GetAllFavoritesByUserID(int id)
+        {
+            string queryString = "SELECT * FROM Favorites WHERE UserID = @id";
+            IEnumerable<Favorite> Favorites = conn.Query<Favorite>(queryString, new { id = id });
+            conn.Close();
             return Favorites;
         }
 
@@ -41,7 +80,7 @@ namespace ClickUniversityGo.Services
         {
             string queryString = "SELECT * FROM Questions";
             IEnumerable<Question> Questions = conn.Query<Question>(queryString);
-
+            conn.Close();
             return Questions;
         }
 
@@ -50,7 +89,7 @@ namespace ClickUniversityGo.Services
         {
             string queryString = "SELECT * FROM UserProfiles";
             IEnumerable<UserProfile> UserProfiles = conn.Query<UserProfile>(queryString);
-
+            conn.Close();
             return UserProfiles;
         }
 
