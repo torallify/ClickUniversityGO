7195165346740d7e8417dee43ab1a65473180159
Sameer Siddiqui <sameersiddiqui@quickenloans.com>
2020-05-20 10:41:51 -0400

    Controllers added

---
+++ ClickUniversityGo/Controllers/QandAController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClickUniversityGo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QandAController : ControllerBase
+    {
+        // GET: api/QandA
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/QandA/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/QandA
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/QandA/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
