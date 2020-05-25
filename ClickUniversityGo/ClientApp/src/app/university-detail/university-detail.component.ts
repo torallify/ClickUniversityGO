0f58fdd1fcc257c048dc9396e69ad07ac6bde3a0
J Fish <JFish-@nexsystech.com>
2020-05-25 14:40:21 -0400

    university-detail method

--- ClickUniversityGo/ClientApp/src/app/university-detail/university-detail.component.ts
+++ ClickUniversityGo/ClientApp/src/app/university-detail/university-detail.component.ts
@@ -1,35 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { UniversitiesDataService } from '../universities-data.service';
 import { FavoritesDataService } from '../favorites-data.service';
 import { University } from '../interfaces/university';
 import { JoinedItem } from '../interfaces/favorite';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 
 @Component({
-    selector: 'app-university-detail',
-    templateUrl: './university-detail.component.html',
-    styleUrls: ['./university-detail.component.scss']
+  selector: 'app-university-detail',
+  templateUrl: './university-detail.component.html',
+  styleUrls: ['./university-detail.component.scss']
 })
 /** university-detail component*/
 export class UniversityDetailComponent {
-/** university-detail ctor */
+  /** university-detail ctor */
 
   university: University;
+  @Input() ref: string;
+  @Input() id: number;
+  route: any;
+
 
   constructor(private universityData: UniversitiesDataService,
     private favoriteData: FavoritesDataService) { }
 
-//  ngOnInit() {
-//    this.route.params.subscribe(params => {
-//      this.id = +params['id'];
-//    })
-//  }
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+      this.getUniversity(this.id);
+    })
+  }
 
-//  getUniversity(id: number) {
-//    this.universityData.getUniversity(id).subscribe(
-//      (data: University) => {
-//        this.getUniversity = data;
-//      },
-//      error => console.error(error)
-//    );
-//  }
+  getUniversity(id: number): Subscription {
+    return this.universityData.getUniversity(id).subscribe(
+      (data: University) => this.university = { ...data },
+      error => console.error(error))
+      }
+
+  //getUniversity(id: number): University {
+  //  this.universityData.getUniversity(id).subscribe(
+  //    (data: University) => this.university = { ...data },
+  //    error => console.error(error))
+  //}
+
 }
