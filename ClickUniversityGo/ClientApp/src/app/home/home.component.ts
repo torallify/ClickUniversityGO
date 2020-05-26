8792e51b376f3d4b7c565b4c4dfe5b36671eccaa
Andrew Steward <andrewsteward@quickenloans.com>
2020-05-26 10:33:43 -0400

    updated tables and home page

--- ClickUniversityGo/ClientApp/src/app/home/home.component.ts
+++ ClickUniversityGo/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { UniversitiesDataService } from '../universities-data.service';
+import { FavoritesDataService } from '../favorites-data.service';
+import { University } from '../interfaces/university';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  universities: University[];
+
+  constructor(private universityData: UniversitiesDataService,
+    private favoriteData: FavoritesDataService) { }
+
+  ngOnInit() {
+    this.get();
+  }
+
+  get() {
+    this.universityData.getAllUniversities().subscribe(
+      (data: University[]) => {
+        this.universities = data;
+      },
+      error => console.error(error)
+    );
+  }
 }
