3f51deaa74eb0aee79737ea43d007e370ef6b59c
Andrew Steward <andrewsteward@quickenloans.com>
2020-05-27 09:38:47 -0400

    Random Universities

--- ClickUniversityGo/ClientApp/src/app/home/home.component.ts
+++ ClickUniversityGo/ClientApp/src/app/home/home.component.ts
@@ -9,12 +9,23 @@ import { University } from '../interfaces/university';
 })
 export class HomeComponent {
   universities: University[];
+  randomUniversity1: number;
+  randomUniversity2: number;
+  randomUniversity3: number;
+  randomUniversity4: number;
+  randomUniversity5: number;
+  
 
   constructor(private universityData: UniversitiesDataService,
     private favoriteData: FavoritesDataService) { }
 
   ngOnInit() {
     this.get();
+    this.getRandomNumber1(1, 1495);
+    this.getRandomNumber2(1, 1495);
+    this.getRandomNumber3(1, 1495);
+    this.getRandomNumber4(1, 1495);
+    this.getRandomNumber5(1, 1495);
   }
 
   get() {
@@ -25,4 +36,53 @@ export class HomeComponent {
       error => console.error(error)
     );
   }
+
+  //getRandomNumber() {
+  //  const randomIndex = Math.floor(Math.random() * this.universities.length);
+  //  const randomUniversity = this.universities[randomIndex];
+  //  this.randomUniversities.push(randomUniversity);
+  //}
+
+  getRandomUniversity() {
+  var unique = true;
+  var num = Math.floor(Math.random() * this.universities.length - 5);
+  var university = this.universities.splice(num, 1);
+  this.universities.push(name);
+  }
+
+  getRandomNumber1(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+    this.randomUniversity1 = result;
+  }
+
+  getRandomNumber2(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+    this.randomUniversity2 = result;
+  }
+
+  getRandomNumber3(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+    this.randomUniversity3 = result;
+  }
+
+  getRandomNumber4(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+    this.randomUniversity4 = result;
+  }
+
+  getRandomNumber5(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+    this.randomUniversity5 = result;
+  }
+
 }
